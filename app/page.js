e23e34b1458385589c9179a0a393e0cdbcb75a24
Md Azad Hossain <mdazadhossain@Mds-MacBook-Pro.local>
2024-04-04 23:51:54 +0200

    added landing page and fixed the get started button route

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
+import LandingPage from "@/components/LandingPage";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Landing page</h1>
-    </main>
+    <LandingPage />
   );
 }
